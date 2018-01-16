8a7b16da4191ccafae3da6d2963390060c75b036
Nebojsa Bozic <nebojsa.bozic@teleboy.ch>
2018-01-16 13:08:14 +0100

    Small changes

--- app/src/main/java/com/example/bookee/weatherinfo/findcity/Presenter.java
+++ app/src/main/java/com/example/bookee/weatherinfo/findcity/Presenter.java
@@ -4,7 +4,6 @@ package com.example.bookee.weatherinfo.findcity;
 import com.example.bookee.weatherinfo.data.CityForecastInfo;
 import com.example.bookee.weatherinfo.data.RetrofitWeatherRepository;
 
-
 import static com.example.bookee.weatherinfo.utils.Constants.CELSIOUS_FAHRENHEIT_DIFFERENCE;
 
 public class Presenter implements MvpContract.Presenter {
@@ -18,6 +17,29 @@ public class Presenter implements MvpContract.Presenter {
     }
 
     @Override
+    public void getData(String desiredCity) {
+        attachedDataInstance.fetchData(desiredCity, new MvpContract.FetchNewCityWeatherInfoCallback() {
+            @Override
+            public void fetchNewWeather(String cityName, CityForecastInfo info) {
+                if (info != null){
+                    String name     = info.getName();
+                    String temp     = String.valueOf(prepareTempForDisplay(info));
+                    String wind     = String.valueOf(info.getWind().getSpeed());
+                    String humidity = String.valueOf(info.getMain().getHumidity());
+                    attachedView.reciveDataFromPresenter(name, temp, wind, humidity);
+                } else {
+                    attachedView.errorHappened("porgesno uneto ime grada");
+                }
+            }
+
+            @Override
+            public void error(Throwable t) {
+                attachedView.errorHappened("GRESKA" + t.toString());
+            }
+        });
+    }
+
+    @Override
     public void bindView(MvpContract.View view) {
        this.attachedView=  view;
     }
@@ -25,35 +47,6 @@ public class Presenter implements MvpContract.Presenter {
     private int prepareTempForDisplay(CityForecastInfo info) {
         return (int) (info.getMain().getTemp() - CELSIOUS_FAHRENHEIT_DIFFERENCE);
     }
-
-   @Override
-   public void getData(String desiredCity) {
-        attachedDataInstance.fetchData(desiredCity, new MvpContract.FetchNewCityWeatherInfoCallback() {
-
-
-            @Override
-            public void fetchNewWeather(String cityName, CityForecastInfo info) {
-               if(info!=null) {
-                   String name = info.getName();
-                   String temp = String.valueOf(prepareTempForDisplay(info));
-                   String wind = String.valueOf(info.getWind().getSpeed());
-                   String humidity = String.valueOf(info.getMain().getHumidity());
-
-                   attachedView.reciveDataFromPresenter(name, temp, wind, humidity);
-               } else {
-                   attachedView.errorHappened("porgesno uneto ime grada");
-               }
-            }
-
-            @Override
-            public void error(Throwable t) {
-
-                attachedView.errorHappened("GRESKA"+t.toString());
-            }
-        });
-
-    }
-
 
 
     @Override
