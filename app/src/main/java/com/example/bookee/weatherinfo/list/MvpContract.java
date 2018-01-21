c23f945d405e7a858142ef2105ae7467aa5d3435
Bukisha <bukisha@gmail.com>
2018-01-21 13:59:26 +0100

    WAP-10 new activity added with baseic classes needed

---
+++ app/src/main/java/com/example/bookee/weatherinfo/list/MvpContract.java
@@ -0,0 +1,15 @@
+package com.example.bookee.weatherinfo.list;
+
+
+public interface MvpContract {
+
+    interface Model {
+
+    }
+    interface Presenter {
+
+    }
+    interface View {
+
+    }
+}
